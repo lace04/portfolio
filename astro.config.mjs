9122bbe41f3a51af16c713ab1a57dd3ef37606d4
lace04 <luisace04@gmail.com>
2024-01-10 11:04:04 -0500

    Refactor astro.config.mjs imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-import vercel from "@astrojs/vercel/serverless";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
-  adapter: vercel()
 });
